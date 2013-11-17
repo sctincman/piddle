0fca989a516dad639ae24a006da8932b6a21e0e3
Reinis Danne <rei4dan@gmail.com>
2013-11-17 21:10:14 +0200

    polyfunc.py: Remove trailing spaces

--- polyfunc.py
+++ polyfunc.py
@@ -1,18 +1,18 @@
 # Christopher Lee (c) 2000
-# code commisioned by Greg Wilson 
+# code commisioned by Greg Wilson
 # draw polynomial functions using bezier exact representations in piddle
 
 # Primary Interface Functions:
 #    drawQuad - draws to a canvas
 #    drawCubicPolynomial
 #    class CoordFrame  - defines a coordinate frame in which to draw
-    
+
 # See the function runtest() for basic use
 
 # Testing:
 #   works w/ piddlePS and piddlePIL
 
-    
+
 
 def quadratic(x,A,B,C):
     # y(x) = A x^2 + B x + C
@@ -37,7 +37,7 @@ def quadPts(xinterval,  A=0., B=0., C=0.):
     xlen = x3-x0
     # print 'xlen = ', xlen
     cx = xlen  # set x scaling, check if makes sense for xlen <0
-    
+
     # convert to cubic paramaters, adjust for xscaling
     # lower case used for cubic: w(t) = a t^3 + b*t^2 + c* t + w0
     # t: [0,1]
@@ -51,7 +51,7 @@ def quadPts(xinterval,  A=0., B=0., C=0.):
     ### print a,b,c,v0
     x = [x0, 0, 0, 0]; y = [y0, 0, 0, 0]
     v = [x, y]
-    for ii in (0, 1): # iterate over vector components 
+    for ii in (0, 1): # iterate over vector components
         v[ii][1] = v[ii][0] + c[ii]/3.0
         v[ii][2] = v[ii][1] + (c[ii]+b[ii])/3.0
         v[ii][3] = v[ii][0] + c[ii] + b[ii] + a[ii]
@@ -60,7 +60,7 @@ def quadPts(xinterval,  A=0., B=0., C=0.):
             v[0][1], v[1][1],
             v[0][2], v[1][2],
             v[0][3], v[1][3])
-    
+
 def cubicPts(xinterval,  A=0., B=0., C=0., D=0):
     """return the control points for a bezier curve that fits the cubic polynomial
     y = A x^3 + B x^2 + C x + D.  for the interval along the x-axis given by
@@ -74,21 +74,21 @@ def cubicPts(xinterval,  A=0., B=0., C=0., D=0):
     xlen = x3-x0
     # print 'xlen = ', xlen
     cx = xlen  # set x scaling, check if makes sense for xlen <0
-    
+
     # convert to cubic paramaters, adjust for xscaling
     # lower case used for cubic: w(t) = a t^3 + b*t^2 + c* t + w0
     # t: [0,1]
     # parameters are adjusted to accout for x(t) = cx*t+x0 tranformation
     #by <-> A
     #cy <-> B
-    a = (0,A*cx*cx*cx) 
+    a = (0,A*cx*cx*cx)
     b = (0, B*cx*cx+ 3*A*cx*cx*x0 )
     c = (cx, C*cx + 2*B*cx*x0+3*A*cx*x0*x0)
     v0 = (x0,y0) ;
     ### print a,b,c,v0
     x = [x0, 0, 0, 0]; y = [y0, 0, 0, 0]
     v = [x, y]
-    for ii in (0, 1): # iterate over vector components 
+    for ii in (0, 1): # iterate over vector components
         v[ii][1] = v[ii][0] + c[ii]/3.0
         v[ii][2] = v[ii][1] + (c[ii]+b[ii])/3.0
         v[ii][3] = v[ii][0] + c[ii] + b[ii] + a[ii]
@@ -141,7 +141,7 @@ class AffineMatrix:
 
     def rightMultiply(self, a, b, c, d, e, f):
         "multiply self.A by matrix M defined by coefficients a,b,c,d,e,f"
-        # 
+        #
 
         #             [    m0*a+m2*b,    m0*c+m2*d, m0*e+m2*f+m4]
         #  ctm * M =  [    m1*a+m3*b,    m1*c+m3*d, m1*e+m3*f+m5]
@@ -161,7 +161,7 @@ class AffineMatrix:
         x,y = pt
         a,b,c,d,e,f = self.A
         return [ a*x + c*y+e, b*x+d*y+f]
-        
+
     def transformFlatList(self, seq):
         # transform a (flattened) sequence of points in form [x0,y0, x1,y1,..., x(N-1), y(N-1)]
         N = len(seq) # assert N even
@@ -194,21 +194,21 @@ class CoordFrame:
 
         Sx = float(w)/uWidth
         Sy = float(h)/uHeight
-        
+
         self.u2dTM = AffineMatrix() # user to device transformation matrix
 
         self.u2dTM.translate(x0, y0)
 
         # if YaxisGoesUp
         self.u2dTM.translate(0,h) # go down this far
-        self.u2dTM.scale(Sx, -Sy)  
-        self.u2dTM.translate(-uX0, -uY0)  
+        self.u2dTM.scale(Sx, -Sy)
+        self.u2dTM.translate(-uX0, -uY0)
 
     def drawFrameBox(self,canvas):
         'Draw a rectangle that represents the "target area" for the coord frame'
         # helps with debugging
         canvas.drawRect(self.x0, self.y0, self.x0+self.w, self.y0 + self.h)
-        
+
 
 
 def drawCircleAt(canvas, x,y,r, **kw):
@@ -251,7 +251,7 @@ def drawQuadWithCtrlPts(canvas, frame, xinterval, A=0.0, B=0.0, C=0.0,
     drawCircleAt(canvas, Tqpts[2], Tqpts[3], 5)
     drawCircleAt(canvas, Tqpts[4], Tqpts[5], 5)
     drawCircleAt(canvas, Tqpts[6], Tqpts[7], 5)
-    
+
 
 def runtest():
     # These are my parameters for the coodinate frame
@@ -262,7 +262,7 @@ def runtest():
     import sys,os, os.path
     sys.path.append(os.path.join(os.getcwd(), "examples"))
     import textClasses
-    
+
     my_x0 = 50
     my_y0 = 50
     my_w  = 300
@@ -272,7 +272,7 @@ def runtest():
     my_uY0 = -100.0
     my_uWidth = 20.0
     my_uHeight = 200.0
-    
+
     frame = CoordFrame(x0= my_x0, y0= my_y0, w=my_w, h=my_h,
                        uX0= my_uX0, uY0= my_uY0,
                        uWidth= my_uWidth,
@@ -290,9 +290,9 @@ def runtest():
         NA = 10.0
         for AA in xrange(1,NA,2):
             drawQuad(canvas, frame, xinterval=(-10,10), A= AA/NA, B=0.0)
-            
+
         # now some other assorted quadratics
-        
+
         drawQuad(canvas, frame, xinterval=(-10,10), A=-1.0, B=0.0, C=0, edgeColor=piddle.red)
         # shift down and add B portion
         drawQuad(canvas, frame, xinterval=(-10,10), A=-.4, B=-3.0, C=-30.0, edgeColor=piddle.blue)
@@ -313,8 +313,8 @@ def runtest():
     #import piddlePIL
     #canvas = piddlePIL.PILCanvas(size=(400,400), name='qtest.png')
     #ToCanvas(canvas,frame)
-    
-        
+
+
 if __name__== '__main__':
     print 'Running test drawing assorted quadratics to qtest.ps'
     import piddle
