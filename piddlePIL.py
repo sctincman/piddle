ac181ca8eda9d1dad76bbafbc910913e44586c81
Reinis Danne <rei4dan@gmail.com>
2013-11-17 22:13:25 +0200

    piddlePIL.py: Remove trailing spaces

--- piddlePIL.py
+++ piddlePIL.py
@@ -1,16 +1,16 @@
 # piddlePIL.py -- a Python Imaging Library backend for PIDDLE
 # Copyright (C) 1999  Joseph J. Strout
-# 
+#
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2 of the License, or (at your option) any later version.
-# 
+#
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -21,7 +21,7 @@ This module implements a Python Imaging Library PIDDLE canvas.
 In other words, this is a PIDDLE backend that renders into a
 PIL Image object.  From there, you can save as GIF, plot into
 another PIDDLE canvas, etc.
-        
+
 Joe Strout (joe@strout.net), 10/26/99
 """
 
@@ -103,7 +103,7 @@ def _pilFont(font):
 
     size = _closestSize(font.size)
     if type(face) == StringType:
-        try: 
+        try:
             pilfont = ImageFont.load_path(_pilFontPath(face,size,font.bold))
         except:
             return 0        # font not found!
@@ -119,7 +119,7 @@ def _pilFont(font):
 
 
 class PILCanvas( Canvas ):
-    
+
     def __init__(self, size=(300,300), name='piddlePIL'):
         self._image = Image.new('RGB',size, (255,255,255))
         import ImageDraw
@@ -128,7 +128,7 @@ class PILCanvas( Canvas ):
         self._setFont( Font() )
         self._pilversion = map(string.atoi, string.split(Image.VERSION, "."))
         Canvas.__init__(self, size, name)
-        
+
     def __setattr__(self, attribute, value):
         self.__dict__[attribute] = value
         if attribute == "defaultLineColor":
@@ -146,7 +146,7 @@ class PILCanvas( Canvas ):
 
     def getImage(self):
         return self._image
-    
+
     def save(self, file=None, format=None):
         """format may be a string specifying a file extension corresponding to
                 an image file format. Ex: 'png', 'jpg', 'gif', 'tif' etc."""
@@ -185,26 +185,26 @@ class PILCanvas( Canvas ):
         out = 0
         for c in s:
             out = out + map[c]
-        return out          
-    
+        return out
+
     def fontAscent(self, font=None):
         "Find the ascent (height above base) of the given font."
 
         if not font: font = self.defaultFont
         if not _ascents: return font.size
-        
+
         path = _matchingFontPath(font)
         return _ascents[path]
-    
+
     def fontDescent(self, font=None):
         "Find the descent (extent below base) of the given font."
 
         if not font: font = self.defaultFont
         if not _descents: return font.size/2
-        
+
         path = _matchingFontPath(font)
         return _descents[path]
-        
+
     #------------- drawing methods --------------
     def drawLine(self, x1,y1, x2,y2, color=None, width=None):
         "Draw a straight line between x1,y1 and x2,y2."
@@ -213,7 +213,7 @@ class PILCanvas( Canvas ):
             if color == transparent: return
             self._setColor(color)
         elif self.defaultLineColor == transparent: return
-        
+
         if width: w = width
         else: w = self.defaultLineWidth
         if w > 1:
@@ -239,7 +239,7 @@ class PILCanvas( Canvas ):
                 pts.append( (x2-hw+w,y2-hw+w) )
                 pts.append( (x2-hw+w,y2-hw) )
                 pts.append( (x2-hw,  y2-hw) )
-                
+
             elif (y1<=y2):              # line down and to the left
                 pts.append( (x1-hw+w,y1-hw+w) )
                 pts.append( (x1-hw+w,y1-hw) )
@@ -248,7 +248,7 @@ class PILCanvas( Canvas ):
                 pts.append( (x2-hw,  y2-hw) )
                 pts.append( (x2-hw,  y2-hw+w) )
                 pts.append( (x2-hw+w,y2-hw+w) )
-                
+
             else:                       # line up and to the left
                 pts.append( (x1-hw,  y1-hw+w) )
                 pts.append( (x1-hw+w,y1-hw+w) )
@@ -257,9 +257,9 @@ class PILCanvas( Canvas ):
                 pts.append( (x2-hw+w,y2-hw) )
                 pts.append( (x2-hw,  y2-hw) )
                 pts.append( (x2-hw,  y2-hw+w) )
-                
+
             self._pen.polygon(pts)
-            
+
         else:
             # for width <= 1, just use fast line method
             # This switch is not good.Must be updated when PIL
@@ -270,7 +270,7 @@ class PILCanvas( Canvas ):
                 self._pen.line( (x1,y1,x2,y2) )
 
 
-    def drawPolygon(self, pointlist, 
+    def drawPolygon(self, pointlist,
                 edgeColor=None, edgeWidth=None, fillColor=None, closed=0):
         """drawPolygon(pointlist) -- draws a polygon
         pointlist: a list of (x,y) tuples defining vertices
@@ -296,14 +296,14 @@ class PILCanvas( Canvas ):
             self._pen.setfill(1)
             self._pen.polygon(pts)
         self._pen.setfill(0)
-            
+
         # set color for edge...
         if edgeColor:
             self._setColor(edgeColor)
         else:
             self._setColor(self.defaultLineColor)
 
-        if edgeColor != transparent:        
+        if edgeColor != transparent:
             # set edge width...
             if edgeWidth == None: edgeWidth = self.defaultLineWidth
 
@@ -325,17 +325,17 @@ class PILCanvas( Canvas ):
     def drawString(self, s, x,y, font=None, color=None, angle=0):
         "Draw a string starting at location x,y."
         if '\n' in s or '\r' in s:
-            self.drawMultiLineString(s, x,y, font, color, angle) 
+            self.drawMultiLineString(s, x,y, font, color, angle)
             return
         if not font: font = self.defaultFont
-        
+
         if not color:
             color = self.defaultLineColor
         if color == transparent: return
 
         # draw into an offscreen Image
         # tmpsize was originally 1.2* stringWidth, added code to give enough room for single character strings (piddle bug#121995)
-        sHeight = (self.fontAscent(font) + self.fontDescent(font)) 
+        sHeight = (self.fontAscent(font) + self.fontDescent(font))
         sWidth = self.stringWidth(s, font)
         tempsize = max(sWidth*1.2, sHeight*2.0)
 
@@ -349,13 +349,13 @@ class PILCanvas( Canvas ):
         pos = [4, int(tempsize/2 - self.fontAscent(font)) - self.fontDescent(font)]
         temppen.text( pos, s )
         pos[1] = int(tempsize/2)
-        
+
         # underline
         if font.underline:
             ydown = (0.5 * self.fontDescent(font) )
             # thickness = 0.08 * font.size # may need to ceil this
             temppen.line([(pos[0], pos[1]+ydown), (pos[0]+sWidth,pos[1]+ydown)])
-            
+
         # rotate
         if angle:
             from math import pi, sin, cos
@@ -365,17 +365,17 @@ class PILCanvas( Canvas ):
             r = tempsize/2 - pos[0]
             pos[0] = int(tempsize/2 - r * cos(radians))
             pos[1] = int(pos[1] - r * sin(radians))
-            
+
         ### temppen.rectangle( (pos[0],pos[1],pos[0]+2,pos[1]+2) ) # PATCH for debugging
         # colorize, and copy it in
         mask = tempimg.convert('L').point(lambda c:c)
         temppen.setink( (color.red*255, color.green*255, color.blue*255) )
         temppen.setfill(1)
         temppen.rectangle( (0,0,tempsize,tempsize) )
-        self._image.paste( tempimg, (int(x)-pos[0],int(y)-pos[1]), mask )       
+        self._image.paste( tempimg, (int(x)-pos[0],int(y)-pos[1]), mask )
 
 
-        
+
     def drawImage(self, image, x1,y1, x2=None,y2=None):
         """Draw a PIL Image into the specified rectangle.  If x2 and y2 are
         omitted, they are calculated from the image size."""
@@ -393,26 +393,26 @@ def test():
     canvas.defaultLineColor = Color(0.7,0.7,1.0)    # light blue
     canvas.drawLines( map(lambda i:(i*10,0,i*10,300), range(30)) )
     canvas.drawLines( map(lambda i:(0,i*10,300,i*10), range(30)) )
-    canvas.defaultLineColor = black     
-    
+    canvas.defaultLineColor = black
+
     canvas.drawLine(10,200, 20,190, color=red)
 
     canvas.drawEllipse( 130,30, 200,100, fillColor=yellow, edgeWidth=4 )
-    
+
     canvas.drawArc( 130,30, 200,100, 45,50, fillColor=blue, edgeColor=navy, edgeWidth=4 )
-    
+
     canvas.defaultLineWidth = 4
     canvas.drawRoundRect( 30,30, 100,100, fillColor=blue, edgeColor=maroon )
     canvas.drawCurve( 20,20, 100,50, 50,100, 160,160 )
-    
-    canvas.drawString("This is a test!", 30,130, Font(face="times",size=16,bold=1), 
+
+    canvas.drawString("This is a test!", 30,130, Font(face="times",size=16,bold=1),
             color=green, angle=-45)
 
     canvas.drawString("This is a test!", 30,130, color=red, angle=-45)
-    
+
     polypoints = [ (160,120), (130,190), (210,145), (110,145), (190,190) ]
     canvas.drawPolygon(polypoints, fillColor=lime, edgeColor=red, edgeWidth=3, closed=1)
-    
+
     canvas.drawRect( 200,200,260,260, edgeColor=yellow, edgeWidth=5 )
     canvas.drawLine( 200,260,260,260, color=green, width=5 )
     canvas.drawLine( 260,200,260,260, color=red, width=5 )
@@ -421,7 +421,7 @@ def test():
     canvas.flush()
     canvas.getImage().save("test.png")
 
-    
+
     return canvas
 
 def testit(canvas, s, x,y, font=None):
@@ -431,22 +431,22 @@ def testit(canvas, s, x,y, font=None):
     w = canvas.stringWidth(s, font=font)
     canvas.drawLine(x,y, x+w,y)
     canvas.drawLine(x,y-canvas.fontAscent(font=font), x+w,y-canvas.fontAscent(font=font))
-    canvas.drawLine(x,y+canvas.fontDescent(font=font), x+w,y+canvas.fontDescent(font=font)) 
-    
+    canvas.drawLine(x,y+canvas.fontDescent(font=font), x+w,y+canvas.fontDescent(font=font))
+
 def test2():
-    
+
     canvas = PILCanvas()
     testit( canvas, "Foogar", 20, 30 )
-    
+
     testit( canvas, "Foogar", 20, 90, font=Font(size=24) )
     global dammit
     dammit = _pilFont(Font(size=24))
-    
+
     testit( canvas, "Foogar", 20, 150, font=Font(face='courier',size=24) )
-    
+
     testit( canvas, "Foogar", 20, 240, font=Font(face='courier') )
-    
-    
+
+
     import piddleQD
     global qdcanvas
     try:
@@ -454,7 +454,7 @@ def test2():
     except: pass
     qdcanvas = piddleQD.QDCanvas()
     qdcanvas.drawImage( canvas.getImage(), 0, 0 );
-    
+
 
 if __name__ == '__main__': test()
-    
+
